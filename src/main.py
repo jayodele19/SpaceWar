e4738a6fdd2ef16a821031f04b6dd912eef25f83
Jayodele19 <jayodele78@yahoo.com>
2022-02-22 13:29:55 -0600

    initial main.py

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import pygame
+import os
+import time
+import random
+redShip = pygame.image.load(os.path.join("src", "pixel_ship_red_small.png"))
+greenShip = pygame.image.load(os.path.join("src", "pixel_ship_green_small.png"))
+yellowShip = pygame.image.load(os.path.join("src", "pixel_ship_yellow.png"))
+blueShip = pygame.image.load(os.path.join("src", "pixel_ship_blue_small.png"))
